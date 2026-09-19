db67477242d57f7ce74af43f0b174d866dddfbce
Egwame Nicholas <nicosheg0123@gmail.com>
2026-09-19 13:33:29 +0100

    harden attendance style regression detector

--- scripts/critical-ui-regression.js
+++ scripts/critical-ui-regression.js
@@ -2,7 +2,7 @@
 import{readFileSync}from'node:fs';
 
 const file=readFileSync('components/AttendanceModal.js','utf8');
-const declaredStyles=new Set([...file.matchAll(/\b(?:const|let|var)\s+([A-Za-z_$][\w$]*)\s*=\s*\{/g)].map(m=>m[1]));
+const declaredStyles=new Set([...file.matchAll(/\b([A-Za-z_$][\w$]*)\s*=\s*\{/g)].map(m=>m[1]));
 const refs=new Set();
 for(const m of file.matchAll(/style=\{([A-Za-z_$][\w$]*)\}/g))refs.add(m[1]);
 for(const m of file.matchAll(/style=\{\{\.\.\.([A-Za-z_$][\w$]*)/g))refs.add(m[1]);
